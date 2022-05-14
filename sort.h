bcd7a40cc33a2bf6ad83c552344ad791892878d7
Becca615 <damilolarebecca8@gmail.com>
2022-05-14 11:46:36 -0700

    All done

--- sort.h
+++ sort.h
@@ -3,10 +3,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-
-/*Data structures*/
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -22,16 +19,21 @@ typedef struct listint_s
 } listint_t;
 
 
-void print_array(const int *, size_t);
-void print_list(const listint_t *);
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
 
-void bubble_sort(int *, size_t);
-void insertion_sort_list(listint_t **);
-void selection_sort(int *, size_t);
-void quick_sort(int *, size_t);
-void shell_sort(int *, size_t);
-void cocktail_sort_list(listint_t **);
-void counting_sort(int *, size_t);
-void merge_sort(int *, size_t);
-void heap_sort(int *, size_t);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+
 #endif /*SORT_H*/
